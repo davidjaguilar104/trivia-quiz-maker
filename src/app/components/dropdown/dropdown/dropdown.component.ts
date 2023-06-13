280fb84d562d5a7f16222c63b5e585102f21466d
davidaguilarcc <david.aguilar@risen-one.com>
2023-06-13 17:14:00 -0500

    use triviaService to log data

--- src/app/components/dropdown/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown/dropdown.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TriviaService } from 'src/app/services/trivia.service';
 
 @Component({
   selector: 'app-dropdown',
   templateUrl: './dropdown.component.html',
-  styleUrls: ['./dropdown.component.scss']
+  styleUrls: ['./dropdown.component.scss'],
 })
-export class DropdownComponent {
-  
+export class DropdownComponent implements OnInit {
+  constructor(private triviaService: TriviaService) {}
+
+  ngOnInit() {
+    this.triviaService.getTriviaCategories().subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
