604be42fedbb69de6aa9a122f8f2f782940887a6
davidjaguilar104 <davidjaguilar104@gmail.com>
2023-06-16 17:50:27 -0500

    break point

--- src/app/components/score/score/score.component.ts
+++ src/app/components/score/score/score.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DropdownComponent } from '../../dropdown/dropdown/dropdown.component';
 
 @Component({
   selector: 'app-score',
   templateUrl: './score.component.html',
-  styleUrls: ['./score.component.scss']
+  styleUrls: ['./score.component.scss'],
 })
-export class ScoreComponent {
+export class ScoreComponent implements OnInit {
+  @ViewChild('quiz') quiz!: DropdownComponent;
 
+  ngOnInit(): void {
+    console.log(this.quiz);
+  }
 }
