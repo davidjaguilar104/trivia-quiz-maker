a70e7fc48a86cb4ce7421f9c3de81a1ef18946d0
davidjaguilar104 <davidjaguilar104@gmail.com>
2023-06-16 17:23:09 -0500

    add a set method for setting questionAnswers

--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -1,28 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
+interface Answer {
+  isSelected: boolean;
+  isCorrect: boolean;
+  answer: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class QuizService {
   questionsAnsweredCount: number = 0;
   activatedEmitter = new Subject<boolean>();
+  questionAnswers: Answer[] = <Answer[]>[];
 
   constructor() {}
 
-  incrementQuestionsAnsweredCount() {
+  incrementQuestionsAnsweredCount(): void {
     this.questionsAnsweredCount++;
     this.isAllQuestionsAnswered();
   }
 
-  isAllQuestionsAnswered() {
+  isAllQuestionsAnswered(): void {
     const isAllQuestionsAnswered = this.questionsAnsweredCount === 5;
     if (isAllQuestionsAnswered) {
       this.activatedEmitter.next(true);
     }
   }
 
-  setQuestionsAnsweredCountToZero() {
+  setQuestionsAnsweredCountToZero(): void {
     this.questionsAnsweredCount = 0;
+  }
+
+  setQuestionsAnswers(questionAnswers: Answer[]): void {
+    this.questionAnswers = questionAnswers; 
   }
 }
